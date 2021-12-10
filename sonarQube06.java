c18229cfb0398c670e4c557de08e8ed7ac603860
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2021-12-10 16:17:59 +0100

    Add files via upload

---
+++ sonarQube06.java
@@ -0,0 +1,11 @@
+public class sonarQube06 {
+    public int chooseOne(int num) {
+        int result;
+        switch (num) {
+            case 0: result = 10; break;
+            default: result = 0; break;
+            case 1: result = 20; break;
+        }
+        return result;
+    }
+}
